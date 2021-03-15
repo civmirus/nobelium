4b9fe1dd49cb549ed370feaf07cf3123adbca33f
civmirus <73327473+civmirus@users.noreply.github.com>
2021-03-15 13:59:31 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,12 +1,12 @@
 const BLOG = {
-  title: 'CRAIGARY',
-  description: 'This gonna be an awesome website.',
-  author: 'Craig Hart',
-  email: 'i@craigary.net',
+  title: 'mirrorball',
+  description: 'shining just for you',
+  author: 'cvmrs',
+  email: 'civmirus@gmail.com',
   link: 'https://craigary.net',
   lang: 'en-US',
   path: '', // leave this empty unless you want to deploy Nobelium in a folder
-  since: 2015, // if leave this empty, current year will be used.
+  since: 2021, // if leave this empty, current year will be used.
   postsPerPage: 7,
   showAbout: true, // WIP
   showArchive: true, // WIP
